466071d0c72f1898588b9795c81912a75d6d0110
preyanshnahar <preyanshnahar200@gmail.com>
2026-05-07 15:31:32 +0530

    Variable, Data Types, and Operators

---
+++ main1.py
@@ -0,0 +1,210 @@
+# print("Hello World") 
+
+# print("Hello World") 
+# print("Hello World") 
+
+# print(23 + 34)
+
+
+#Python is a case sensitive language
+
+
+
+# name = "Preyansh"
+# age = 21 
+# cash = 123456789.12
+
+# print(name) 
+# print(age) 
+# print(cash) 
+
+# age2 = 23
+# print(age2) 
+# age = 2
+# print(age2)
+# Here age2 will not change value since at time of passing the value of age to age2 only a copy is passed and not reference
+
+
+#Rules for identifiers
+#1 Upper,Lower,Numbers,Underscore --> all are allowed
+#2 Cannot start with number
+#3 Can be any length
+#4 Cannot use special characters
+
+
+
+# Types of Data
+# name = "Preyansh" 
+# Strings can be written in single, double and triple quotes
+# print(type(name)) # String
+# age = 21
+# print(type(age)) # int
+# gpa = 8.9
+# print(type(gpa)) # float
+# f = True
+# print(type(f))
+# a = None
+# print(type(a))
+
+
+
+
+
+
+# Python Reserved Words / Key Words
+# False
+# None
+# True
+# and
+# as
+# assert
+# async
+# await
+# break
+# case
+# class
+# continue
+# def
+# del
+# elif
+# else
+# except
+# finally
+# for
+# from
+# global
+# if
+# import
+# in
+# is
+# lambda
+# match
+# nonlocal
+# not
+# or
+# pass
+# raise
+# return
+# try
+# while
+# with
+# yield
+
+
+
+
+
+
+# Print Sum
+# a = 10
+# b = 21
+# c = a + b
+# print(c)
+
+
+
+
+# # -> Single line comment
+# ''' .... ''' Multi Line Comment
+
+
+
+
+# Type of Operators
+# Arithmetic Operators -> *,+,-,/,%,**
+# Relational / Comparison Operators -> ==,!=,>,<,>=,<=
+# Assignment Operators -> = , += , -= , *= , /= , %= , **=
+# Logical Operators -> not,and,or
+
+# Note : While Dividing 2 numbers python always give float value even if value is int
+# a = 10
+# b = 2
+# print(a/b)
+# b = 3
+# print(a/b)
+
+
+
+
+
+
+
+# # Type Conversion -> Automatically Done by Python
+# a = 10
+# b = 20.12
+# print(a+b)
+
+# # Type Casting -> Manually  -> int() , float() , str() , bool()
+# a = 10
+# b = "20"
+# print(a+int(b))
+
+
+
+
+
+
+
+
+# # Taking Input -> input
+# a = input("Enter Your Name : ")
+
+# b = input()
+# print(a+b) 
+
+# # input() -> always stores as string type
+
+# a = input("Enter") 
+# print(type(a)) 
+ 
+# a = int(input("Enter"))
+# print(type(a))  
+
+
+
+
+
+
+
+
+
+
+
+
+# Take 2 numbers input and print their sum
+# a = int(input("Enter num1 : "))
+# b = int(input("Enter num2 : "))
+# sum = a + b 
+# print("Sum = ", sum)
+
+
+
+
+
+
+
+# Take input side of a square and print their output
+# a = float(input("Enter Side : "))
+# area = a * a 
+# print("Area = ",area)
+
+
+
+
+
+# Take 2 floating point numbers and print their average
+# a = float(input("Enter num1 : "))
+# b = float(input("Enter num2 : "))
+# avg = (a + b)/ 2
+# print("Average : ",avg)
+
+
+
+
+
+
+# Print true if a >= b else false
+a = int(input("Enter num1 : "))
+b = int(input("Enter num2 : "))
+print(a >= b)
+
